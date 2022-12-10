0b2f0147a970c2be126703a657fc7b7d5518e7fb
Niko_Lua <1209424096@qq.com>
2022-12-11 03:13:40 +0800

    Add files via upload

---
+++ Ball_Test.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("Hello world!");
+        Debug.LogWarning("这是一个警告！");
+        Debug.LogError("这是一个警告！");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
